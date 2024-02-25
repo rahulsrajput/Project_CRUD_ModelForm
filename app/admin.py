858ecdd1998325c5f814319e5a708afa3571c08d
rahulsrajput <rahulsrajput29@gmail.com>
2024-02-25 08:51:34 +0530

    Added model and registered that

--- app/admin.py
+++ app/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import Employee
 
 # Register your models here.
+@admin.register(Employee)
+class EmployeeAdmin(admin.ModelAdmin):
+    list_display = ['id','first_name','last_name','email','salary','joining_date']
--- app/models.py
+++ app/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+class Employee(models.Model):
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+    email = models.EmailField()
+    salary = models.IntegerField()
+    joining_date = models.DateField()
