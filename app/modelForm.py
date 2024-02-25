d1aca79857a5add97b823987366f25b8a23a8b03
rahulsrajput <rahulsrajput29@gmail.com>
2024-02-25 10:49:08 +0530

    Added html classes to ModelForm 'form'

--- app/modelForm.py
+++ app/modelForm.py
@@ -1,7 +1,16 @@
 from .models import Employee
 from django.forms import ModelForm
+from django import forms
 
 class EmployeeModelForm(ModelForm):
     class Meta:
         model = Employee
         fields = ['first_name','last_name','email','salary','joining_date']
+
+        widgets = {
+            'first_name':forms.TextInput(attrs={'class':'form-control'}),
+            'last_name':forms.TextInput(attrs={'class':'form-control'}),
+            'email':forms.EmailInput(attrs={'class':'form-control'}),
+            'salary':forms.NumberInput(attrs={'class':'form-control'}),
+            'joining_date':forms.DateInput(attrs={'class':'form-control'})
+        }
