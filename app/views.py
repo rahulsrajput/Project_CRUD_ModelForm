d5a6e0e02f7586e502e8c525d98729ba37296536
rahulsrajput <rahulsrajput29@gmail.com>
2024-02-25 09:02:35 +0530

    basic setup of urls & views

--- app/views.py
+++ app/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
 
 # Create your views here.
+def home(request):
+    pass
+
+
+def create(request):
+    pass
+
+
+def update(request):
+    pass
+
+
+def delete(request):
+    pass
--- crud/urls.py
+++ crud/urls.py
@@ -17,6 +17,16 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+from app import views
+
 urlpatterns = [
     path('admin/', admin.site.urls),
+
+    path('', views.home, name='home'),
+
+    path('create/', views.create, name='create'),
+
+    path('update/<int:id>', views.update, name='update'),
+
+    path('delete/<int:id>', views.delete, name='delete'),
 ]
