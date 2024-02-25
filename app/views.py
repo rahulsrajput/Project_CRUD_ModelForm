4eadf24994d4027c88a389cb3980a27d7d598cbf
rahulsrajput <rahulsrajput29@gmail.com>
2024-02-25 09:48:29 +0530

    Created create view & template

--- app/views.py
+++ app/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
-
+from .modelForm import EmployeeModelForm
 from .models import Employee
+from django.http import HttpResponseRedirect
+
 # Create your views here.
 def home(request):
     emps = Employee.objects.all()
@@ -8,7 +10,22 @@ def home(request):
 
 
 def create(request):
-    pass
+    if request.method == 'POST':
+        form = EmployeeModelForm(request.POST)
+        if form.is_valid():
+            firstName = form.cleaned_data['first_name']
+            lastName = form.cleaned_data['last_name']
+            email = form.cleaned_data['email']
+            salary = form.cleaned_data['salary']
+            date = form.cleaned_data['joining_date']
+
+            emp = Employee(first_name = firstName, last_name=lastName, email=email, salary=salary, joining_date=date)
+            emp.save()
+
+            return HttpResponseRedirect('/')
+
+    form = EmployeeModelForm()
+    return render(request, 'create.html',context={'form':form})
 
 
 def update(request):
