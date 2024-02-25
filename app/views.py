1eaf75f90e795af946d1c2ba3af18514d7b01f5b
rahulsrajput <rahulsrajput29@gmail.com>
2024-02-25 09:40:47 +0530

    Created Home View & Template

--- app/views.py
+++ app/views.py
@@ -1,8 +1,10 @@
 from django.shortcuts import render
 
+from .models import Employee
 # Create your views here.
 def home(request):
-    pass
+    emps = Employee.objects.all()
+    return render(request,'home.html', context={'emps':emps})
 
 
 def create(request):
